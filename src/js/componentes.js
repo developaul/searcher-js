e7242e47bdfc0b9fe0678f7ff232d039f7084dcb
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-09 20:52:30 -0500

    función fillSelect agregado llena las opciones del selector year

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,3 +3,31 @@ import '../css/skeleton.css';
 
 import { autos } from './db.js';
 
+
+// References
+const year  = document.querySelector( '#year' ),
+      max   = new Date().getFullYear(),
+      min   = max - 10;
+
+
+
+// Functions
+// Llena el selector de year con opciones de los últimos 10 años
+const fillSelect = () => {
+    for( let i = max; i >= min; i-- ) {
+        const option = document.createElement( 'option' );
+        option.textContent = i;
+        option.value = i;
+        year.appendChild( option );
+    }
+}
+
+
+
+// Events
+export const startEventListeners = () => {
+    document.addEventListener( 'DOMContentLoaded', () => {
+
+        fillSelect();
+    });
+}
